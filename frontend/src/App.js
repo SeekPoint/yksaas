c91bba946c82221b73da05230d02535c931ef74d
yuanke <yuanke@microsoft.com>
2022-11-02 19:58:21 +0800

    add signup and login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Root from "./Root";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Signup from "./components/signup/Signup";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Root>
+          <Switch>
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Root>
+      </div>
+    );
+  }
 }
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <BrowserRouter>
+//           <Routes>
+//               <Route path="/signup" component={<Signup/>} />
+//               <Route path="/login" component={<Login/>} />
+//               <Route path="/dashboard" component={<Dashboard/>} />
+//               <Route path="/" component={<Home/>} />
+//           </Routes>
+//         </BrowserRouter>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
+
+//https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// ...
+// add this import in frontend/src/index.js
+// add it before index.css import
+import "bootstrap/dist/css/bootstrap.css";
+// ...
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
