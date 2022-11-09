84aca7a55d38943dd06b4596d5b83c260caa921d
yuanke <yuanke@microsoft.com>
2022-11-09 20:46:33 +0800

    add auth in react --not perfect

--- frontend/src/Reducer.js
+++ frontend/src/Reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 
-import { loginReducer } from "./components/login/LoginReducer";
+import { signupReducer } from "./components/signup/SignupReducer";
+import { loginReducer } from "./components/login/LoginReducer"; // add import
 
 const createRootReducer = history =>
-  combineReducers({
-    router: connectRouter(history),
-    auth: loginReducer
-  });
+    combineReducers({
+        router: connectRouter(history),
+        createUser: signupReducer,
+        auth: loginReducer // <--- add reducer
+    });
 
 export default createRootReducer;
